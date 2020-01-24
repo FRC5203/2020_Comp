583bfc29b2816038f377072b5b40e6f3a8bbdd7f
Grape <meridian5203@gmail.com>
2020-01-23 19:44:51 -0500

    Initial commit

---
+++ src/main/java/frc/robot/CW_Spinner.java
@@ -0,0 +1,30 @@
+package frc.robot;
+
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+public class CW_Spinner {
+
+    public static WPI_TalonSRX talon = new WPI_TalonSRX(4);
+
+    /**
+    * Number of counts that equals one revolution of motor.
+    * Counts = Encoder position
+    */
+    public static final int COUNTS_PER_REV = 4096;
+    
+    public static void init(){
+        talon.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative,0,10);
+        talon.setSelectedSensorPosition(0);
+    }
+
+    public static void telemetryUpdate(){
+        System.out.println("Rots: " + getRotations());
+        System.out.println("Counts: " + talon.getSelectedSensorPosition());
+        System.out.println("Vel: " + talon.getSelectedSensorVelocity());
+    }
+
+    public static float getRotations(){
+        return (float)talon.getSelectedSensorPosition() / (float)COUNTS_PER_REV;
+    }
+}
